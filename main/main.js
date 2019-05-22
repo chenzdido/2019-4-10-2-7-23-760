a1b27be2cfde284c2bf7c18898eae0182fc34f82
chenz <chenzhedido@outlook.com>
2019-05-22 21:09:32 +0800

    add price

--- main/main.js
+++ main/main.js
@@ -1,4 +1,16 @@
 module.exports = function main(inputs) {
     // write your code here...
-    return "price";
+    var price=0;
+	price=inputs.parkTime*0.25+6;
+	dis=inputs.distance;
+	if(dis>2&&dis<=8)
+	{
+		price+=(dis-2)*0.8;
+	}
+	else if(dis>8)
+    {
+		price=price+4.8+(dis-8)*1.2;
+	}
+	price=Math.round(price);
+	return price;
 };
